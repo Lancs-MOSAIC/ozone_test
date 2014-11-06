1d7788f8d33c43774bca38cf56eadfeb7433f2b7
Andrew Senior <a.senior@lancaster.ac.uk>
2014-11-06 10:06:00 +0000

    Initial version using pthread to move spectrum calculation into a separate thread.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = gcc-4.7 # for better vectorisation support
-CFLAGS=-mfpu=neon -funsafe-math-optimizations -O3 -Wall -std=c99
+CFLAGS=-mfpu=neon -funsafe-math-optimizations -O3 -Wall -std=c99 -fdump-tree-vect
 
 ozone_test: ozone_test.c
-	$(CC) -o ozone_test ozone_test.c -lrtlsdr -lfftw3f -lm $(CFLAGS)
+	$(CC) -o ozone_test ozone_test.c -lrtlsdr -lfftw3f -lm -lpthread  $(CFLAGS)
--- ozone_test.c
+++ ozone_test.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <math.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sched.h>
+#include <sys/mman.h>
 
 #include "rtl-sdr.h"
 #include <fftw3.h>
@@ -21,16 +25,23 @@ int dongle_debug = 1;
 
 #define READ_SIZE (16384 * 256)
 #define NUM_BLOCKS 5
+#define SIG_SIZE (NUM_BLOCKS * READ_SIZE)
 #define NUM_SIG_SPEC 8
 #define FFT_LEN 1024
+#define MAX_IN_QUEUE_LEN 3
 
-uint8_t data_buf[READ_SIZE * NUM_BLOCKS];
-float spec_buf[FFT_LEN];
+uint8_t data_buf[SIG_SIZE * MAX_IN_QUEUE_LEN];
+uint8_t cal_data_buf[READ_SIZE];
+float cal_spec_buf[FFT_LEN];
+float sig_spec_buf[FFT_LEN * NUM_SIG_SPEC * 2];
 float convtab[256];
 
-fftwf_complex *fftin;
-fftwf_complex *fftout;
-fftwf_plan fplan;
+pthread_mutex_t in_queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t in_queue_cond = PTHREAD_COND_INITIALIZER;
+int in_queue_len = 0;
+pthread_mutex_t out_queue_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t out_queue_cond = PTHREAD_COND_INITIALIZER;
+int out_queue_len = 0;
 
 FILE *init_cal_control(void)
 { 
@@ -268,6 +279,87 @@ double find_freq_error(float *calspec, double samplerate, double centfreq,
   return freqerr;
 }
 
+void *comp_thread(void *ptarg)
+{
+  fftwf_plan cfplan;
+  fftwf_complex *cfftin, *cfftout;
+  int r, in_queue_out_ptr = 0, out_queue_in_ptr = 0;
+
+  fprintf(stderr, "  comp_thread: computation thread alive\n");
+
+  cfplan = init_fft(&cfftin, &cfftout);
+  if (cfplan == NULL) {
+    fprintf(stderr, "  comp_thread: failed to initialise FFT\n");
+    return NULL;
+  }
+
+
+
+  while (1) {
+
+    r = pthread_mutex_lock(&in_queue_mutex);
+    if (r != 0) {
+      fprintf(stderr, "  comp_thread: pthread_mutex_lock: %s\n", strerror(r));
+      return NULL;
+    }
+
+    while (in_queue_len == 0) {
+      fprintf(stderr, "  comp_thread: waiting for data\n");
+      r = pthread_cond_wait(&in_queue_cond, &in_queue_mutex);
+      if (r != 0) {
+	fprintf(stderr, "  comp_thread: pthread_cond_wait: %s\n", strerror(r));
+	return NULL;
+      }
+    }
+
+    in_queue_len--;
+
+    r = pthread_mutex_unlock(&in_queue_mutex);
+    if (r != 0) {
+      fprintf(stderr, "  comp_thread: pthread_mutex_unlock: %s\n", strerror(r));
+      return NULL;
+    }
+
+    fprintf(stderr, "  comp_thread: calculating spectrum (%d)\n", 
+	    in_queue_out_ptr);
+
+    calc_spectrum(&data_buf[in_queue_out_ptr * SIG_SIZE], SIG_SIZE, \
+		  &sig_spec_buf[out_queue_in_ptr * FFT_LEN], \
+		  cfplan, cfftin, cfftout);
+
+    in_queue_out_ptr = (in_queue_out_ptr + 1) % MAX_IN_QUEUE_LEN;
+    out_queue_in_ptr = (out_queue_in_ptr + 1) % (NUM_SIG_SPEC * 2);
+
+    r = pthread_mutex_lock(&out_queue_mutex);
+    if (r != 0) {
+      fprintf(stderr, "  comp_thread: pthread_mutex_lock: %s\n", strerror(r));
+      return NULL;
+    }
+
+    out_queue_len++;
+
+    r = pthread_mutex_unlock(&out_queue_mutex);
+    if (r != 0) {
+      fprintf(stderr, "  comp_thread: pthread_mutex_unlock: %s\n", strerror(r));
+      return NULL;
+    }
+
+    r = pthread_cond_signal(&out_queue_cond);
+    if (r != 0) {
+      fprintf(stderr, "pthread_cond_signal: %s\n", strerror(r));
+      return NULL;
+    }
+
+  }
+
+  fftwf_free(cfftin);
+  fftwf_free(cfftout);
+  fftwf_destroy_plan(cfplan);
+
+  return NULL;
+
+}
+
 
 int main(void)
 {
@@ -276,7 +368,11 @@ int main(void)
   int n, r, n_read, write_data = 0;
   double freq_err;
   uint32_t line_rx_freq;
-
+  pthread_t cthread;
+  int in_queue_in_ptr = 0, out_queue_out_ptr = 0;
+  fftwf_complex *fftin;
+  fftwf_complex *fftout;
+  fftwf_plan fplan;
 
   write_data = !isatty(STDOUT_FILENO);
   if (!write_data)
@@ -293,18 +389,40 @@ int main(void)
   if ((calfp = init_cal_control()) == NULL)
     return 1;
 
+  r = pthread_create(&cthread, NULL, comp_thread, NULL);
+  if (r != 0) {
+    fprintf(stderr, "pthread_create(): %s", strerror(r));
+    return 1;
+  }
+
+  /* set RT scheduling for this thread */
+
+  struct sched_param spar;
+  spar.sched_priority = 99;
+  r = pthread_setschedparam(pthread_self(), SCHED_FIFO, &spar);
+  if (r != 0) {
+    fprintf(stderr, "WARNING: could not set RT scheduling: %s\n", \
+	    strerror(r));
+  }
+
+  r = mlockall(MCL_CURRENT | MCL_FUTURE);
+  if (r != 0) {
+    perror("Could not lock memory");
+  }
+
+
   while (1) {
 
     set_frequency(dev, CALRXFREQ);
 
-    memset(data_buf, 0, sizeof(data_buf)); 
+    memset(cal_data_buf, 0, sizeof(cal_data_buf)); 
 
     fprintf(stderr, "  Calibrator on\n");
     set_cal_state(calfp, 1);
 
     rtlsdr_reset_buffer(dev); /* flush any old signal away */
 
-    r = rtlsdr_read_sync(dev, data_buf, READ_SIZE, &n_read);
+    r = rtlsdr_read_sync(dev, cal_data_buf, READ_SIZE, &n_read);
     if (r < 0)
       fprintf(stderr, "WARNING: rtlsdr_read_sync() failed\n");
     if (n_read != READ_SIZE)
@@ -315,33 +433,45 @@ int main(void)
     set_cal_state(calfp, 0);
 
     fprintf(stderr, "  Calculating spectrum... ");
-    calc_spectrum(data_buf, READ_SIZE, spec_buf, fplan, fftin, fftout);
+    calc_spectrum(cal_data_buf, READ_SIZE, cal_spec_buf, fplan, fftin, fftout);
     fprintf(stderr, "Done.\n");
 
     if (write_data) {
-      if (fwrite(spec_buf, sizeof(spec_buf), 1, stdout) != 1)
+      if (fwrite(cal_spec_buf, sizeof(cal_spec_buf), 1, stdout) != 1)
 	fprintf(stderr, "Error writing data to stdout\n");
     }
 
-    freq_err = find_freq_error(spec_buf, SAMPLERATE, CALRXFREQ, CALFREQ);
+    freq_err = find_freq_error(cal_spec_buf, SAMPLERATE, CALRXFREQ, CALFREQ);
 
-    for (int scount = 0; scount < NUM_SIG_SPEC; scount++) {
+    for (int scount = 0; scount < 2 * NUM_SIG_SPEC; scount++) {
 
-      /* tune above line frequency */
+      if ((scount % 2) == 0) {
 
-      line_rx_freq = (uint32_t)((double)LINEFREQ + (double)(SAMPLERATE / 4) \
+	/* tune above line frequency */
+	
+	line_rx_freq = (uint32_t)((double)LINEFREQ + (double)(SAMPLERATE / 4) \
+				  + freq_err);
+      } else {
+	/* tune below line frequency */
+
+	line_rx_freq = (uint32_t)((double)LINEFREQ - (double)(SAMPLERATE / 4) \
 				 + freq_err);
+      }
+
       set_frequency(dev, line_rx_freq);
 
-      fprintf(stderr, "  Recording signal\n");
+      fprintf(stderr, "  main_thread: recording signal %d, %d\n", scount,
+	      in_queue_in_ptr);
 
-      memset(data_buf, 0, sizeof(data_buf));
+      memset(&data_buf[in_queue_in_ptr * SIG_SIZE], 0, SIG_SIZE);
 
       rtlsdr_reset_buffer(dev); /* flush any cal signal away */
 
       for(n = 0; n < NUM_BLOCKS; n++) {
 
-	r = rtlsdr_read_sync(dev, data_buf + n * READ_SIZE, READ_SIZE, &n_read);
+	r = rtlsdr_read_sync(dev, &data_buf[in_queue_in_ptr * SIG_SIZE
+					    + n * READ_SIZE],
+			     READ_SIZE, &n_read);
 	if (r < 0)
 	  fprintf(stderr, "WARNING: rtlsdr_read_sync() failed\n");
 	if (n_read != READ_SIZE)
@@ -350,50 +480,84 @@ int main(void)
 
       }
 
-      fprintf(stderr, "  Calculating spectrum... ");
-      calc_spectrum(data_buf, NUM_BLOCKS * READ_SIZE, spec_buf, fplan, \
-		    fftin, fftout);
-      fprintf(stderr, "Done.\n");
-
-      if (write_data) {
-	if (fwrite(spec_buf, sizeof(spec_buf), 1, stdout) != 1)
-	  fprintf(stderr, "Error writing data to stdout\n");
+      r = pthread_mutex_lock(&in_queue_mutex);
+      if (r != 0) {
+	fprintf(stderr, "pthread_mutex_lock: %s\n", strerror(r));
+	return 1;
       }
 
-      /* tune below line frequency */
+      in_queue_len++;
+      in_queue_in_ptr = (in_queue_in_ptr + 1) % MAX_IN_QUEUE_LEN;
 
-      line_rx_freq = (uint32_t)((double)LINEFREQ - (double)(SAMPLERATE / 4) \
-				 + freq_err);
-      set_frequency(dev, line_rx_freq);
-
-      fprintf(stderr, "  Recording signal\n");
-
-      memset(data_buf, 0, sizeof(data_buf));
-
-      rtlsdr_reset_buffer(dev); /* flush any cal signal away */
-
-      for(n = 0; n < NUM_BLOCKS; n++) {
-
-	r = rtlsdr_read_sync(dev, data_buf + n * READ_SIZE, READ_SIZE, &n_read);
-	if (r < 0)
-	  fprintf(stderr, "WARNING: rtlsdr_read_sync() failed\n");
-	if (n_read != READ_SIZE)
-	  fprintf(stderr, "WARNING: received wrong number of samples (%d)\n", \
-		  n_read);    
-
+      r = pthread_mutex_unlock(&in_queue_mutex);
+      if (r != 0) {
+	fprintf(stderr, "pthread_mutex_unlock: %s\n", strerror(r));
+	return 1;
       }
 
-      fprintf(stderr, "  Calculating spectrum... ");
-      calc_spectrum(data_buf, NUM_BLOCKS * READ_SIZE, spec_buf, fplan, \
-		    fftin, fftout);
-      fprintf(stderr, "Done.\n");
-
-      if (write_data) {
-	if (fwrite(spec_buf, sizeof(spec_buf), 1, stdout) != 1)
-	  fprintf(stderr, "Error writing data to stdout\n");
+      
+      r = pthread_cond_signal(&in_queue_cond);
+      if (r != 0) {
+	fprintf(stderr, "pthread_cond_signal: %s\n", strerror(r));
+	return 1;
       }
+      
+      
+
+      if (in_queue_len >= MAX_IN_QUEUE_LEN)
+	fprintf(stderr, "WARNING: buffer overrun!\n");
   
     }
+
+    /*
+    r = pthread_cond_signal(&in_queue_cond);
+    if (r != 0) {
+      fprintf(stderr, "pthread_cond_signal: %s\n", strerror(r));
+      return 1;
+    }
+    */
+    
+
+    /* Read signal spectra from queue and store them */
+
+    for (int scount = 0; scount < 2 * NUM_SIG_SPEC; scount++) {
+
+      r = pthread_mutex_lock(&out_queue_mutex);
+      if (r != 0) {
+	fprintf(stderr, "pthread_mutex_lock: %s\n", strerror(r));
+	return 1;
+      }
+
+      while (out_queue_len == 0) {
+	fprintf(stderr, "  main_thread: waiting for data\n");
+	r = pthread_cond_wait(&out_queue_cond, &out_queue_mutex);
+	if (r != 0) {
+	  fprintf(stderr, "pthread_cond_wait: %s\n", strerror(r));
+	  return 1;
+	}
+      }
+
+      out_queue_len--;
+
+      r = pthread_mutex_unlock(&out_queue_mutex);
+      if (r != 0) {
+	fprintf(stderr, "pthread_mutex_unlock: %s\n", strerror(r));
+	return 1;
+      }
+
+      if (write_data) {
+
+	if (fwrite(sig_spec_buf + FFT_LEN * out_queue_out_ptr,
+		   FFT_LEN * sizeof(float), 1, stdout) != 1)
+	  fprintf(stderr, "WARNING: could not write out data\n");
+
+      }
+
+      out_queue_out_ptr = (out_queue_out_ptr + 1) % (2 * NUM_SIG_SPEC);
+
+
+    }
+
   }
 
   
